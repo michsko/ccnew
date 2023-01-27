cd170fbf9495fe7f2c99ff9787a7f7e40a4a7be5
michsko <michal.skocovsky@protonmail.com>
2023-01-27 07:04:45 +0100

    navbar connection

--- circus/info/views.py
+++ circus/info/views.py
@@ -5,7 +5,7 @@ from .models import Obec
 # Create your views here.
 
 def circus(request):
-	map1 = folium.Map(location=[49.5, 16], tiles="CartoDB Dark_Matter", zoom_start=7)
+	map1 = folium.Map(location=[49.5, 16], tiles="CartoDB Dark_Matter", zoom_start=9)
 	
 	obce = Obec.objects.all()
 
