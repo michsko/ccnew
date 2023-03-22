389bb2057adddcdd09ef1edc6360fba7119b0d26
michal skocovsky <89877219+michsko@users.noreply.github.com>
2023-03-22 11:25:17 +0100

    Update settings.py

--- circus/circus/settings.py
+++ circus/circus/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!fd^c99md*t^+l!h4%a-x68!p(tib$-!wx$s=92k20xlb0*3+v'
-
+SECRET_KEY = 'xxxxxxxxxx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
